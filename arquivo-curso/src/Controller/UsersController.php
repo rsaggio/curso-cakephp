8cd78c0d54b8e2342c58d398217ed69c69d7558f
Renan Saggio <renan.saggio@gmail.com>
2015-08-10 22:06:44 -0300

    Capitulo 8 aula 3

--- arquivo-curso/src/Controller/UsersController.php
+++ arquivo-curso/src/Controller/UsersController.php
@@ -2,8 +2,14 @@
 namespace App\Controller;
 
 use Cake\ORM\TableRegistry;
+use Cake\Event\Event;
 
 class UsersController extends AppController {
+
+	public function beforeFilter(Event $event) {
+		parent::beforeFilter($event);
+		$this->Auth->allow(['add','save']);
+	}
 
 	public function add() {
 		$usersTable = TableRegistry::get('Users');
@@ -25,7 +31,22 @@ class UsersController extends AppController {
 	}
 
 	public function login() {
-		
+
+		if($this->request->is('post')) {
+			$user = $this->Auth->identify();
+
+			if($user) {
+				$this->Auth->setUser($user);
+				return $this->redirect($this->Auth->redirectUrl());
+			}else {
+				$this->Flash->set('Usuário ou senha inválidos',['element' => 'error']);
+			}
+		}
+
+	}
+
+	public function logout() {
+		return $this->redirect($this->Auth->logout());
 	}
 }
 
