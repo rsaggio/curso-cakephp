e1a22ba7e1ab413f497ccc6d1263981e515eb018
Renan Saggio <renan.saggio@gmail.com>
2015-08-13 20:25:05 -0300

    Capitulo 2 Video 1

--- arquivo-curso/src/Model/Table/ProdutosTable.php
+++ arquivo-curso/src/Model/Table/ProdutosTable.php
@@ -2,9 +2,22 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class ProdutosTable extends Table {
 	
+	public function validationDefault(Validator $validator) {
+
+		$validator->requirePresence('nome',true)->notEmpty('nome');		
+		$validator->add('descricao',[
+			'minLenght' => [
+				'rule' => ['minLength',10],
+				'message' => 'A descrição deve ter pelo menos 10 caracteres'
+			]
+		]);
+		
+		return $validator;
+	}
 }
 
 
